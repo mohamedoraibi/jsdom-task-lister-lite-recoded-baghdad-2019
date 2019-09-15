d3a980063e1a84f968e715837a78ca492c98741b
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-09-15 19:41:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-document.getElementById("create-task-form").addEventListener("submit", updateList);
-
-function updateList(){
-  
-  let inputValue=document.getElementById('new-task-description').value;
-  var ul = document.getElementById("tasks");
-  var li = document.createElement("li");
-    li.appendChild(document.createTextNode(inputValue));
-  ul.appendChild(li);
-}
 
 
 document.querySelector("#create-task-form").addEventListener("submit", function(event) {
