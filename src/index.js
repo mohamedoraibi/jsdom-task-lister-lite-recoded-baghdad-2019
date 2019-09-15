e43508fff0d7e04bb36f74c0b945824bed289f65
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-09-15 19:42:36 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+
+document.querySelector("#create-task-form").addEventListener("submit", function(event) {
+         let inputValue=document.getElementById('new-task-description').value;
+  var ul = document.getElementById("tasks");
+  var li = document.createElement("li");
+    li.appendChild(document.createTextNode(inputValue));
+  ul.appendChild(li);
+         event.preventDefault();
+}, false);
+
+
